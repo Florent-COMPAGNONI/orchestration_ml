d68ee5faa3ceadc8f60226183750db3ecaf68488
Poupoulpe <nievjimmy3@gmail.com>
2024-06-28 15:10:18 +0200

    Add new features in app.py

--- front/app.py
+++ front/app.py
@@ -6,6 +6,7 @@ import time
 # Define the API URL
 API_URL = "https://weather-api-cv3s3i6o4q-od.a.run.app/predict/"
 
+
 def fetch_temperature_data():
     """Fetches temperature data from the API."""
     response = requests.get(API_URL)
@@ -15,11 +16,18 @@ def fetch_temperature_data():
         st.error("Failed to fetch data from the API")
         return []
 
+
 def main():
     st.title('Temperature Predictions for Today')
 
     # Fetching temperature data from the API
-    temperatures = fetch_temperature_data().get('predictions')
+    fetch = fetch_temperature_data()
+
+    if isinstance(fetch, list):
+        temperatures = fetch.get('temperatures')
+    else:
+        temperatures = [round(float(temp)) for temp in
+                        fetch.get('predictions').replace("[", "").replace("]", "").replace(" ", "").split(",")]
 
     if temperatures:
         # Creating a DataFrame for visualization
@@ -28,13 +36,21 @@ def main():
             'Temperature': temperatures
         })
 
-        # Displaying the temperature data as a line chart
+        hour = st.sidebar.slider("hour", 0, 23)
+        hour_string = str(hour % 12) + ' pm' if hour > 12 else str(hour) + ' am'
+
+        st.header('Look close by hour', divider='rainbow')
+        st.subheader(f"At {hour_string} :sunglasses:")
+        temp = temperatures[hour]
+        st.markdown(f"The temperature should be at ***{temp} °C***")
+
+        st.header('About today\'s temperature', divider='rainbow')
         st.subheader('Line Chart of Temperatures')
-        st.line_chart(df.set_index('Hour'))
+        st.line_chart(data=df.set_index('Hour'), y_label="Temperature", x_label="Hour")
 
         # Adding a bar chart
         st.subheader('Bar Chart of Temperatures')
-        st.bar_chart(df.set_index('Hour'))
+        st.bar_chart(df.set_index('Hour'), y_label="Temperature", x_label="Hour")
 
 
 if __name__ == "__main__":
